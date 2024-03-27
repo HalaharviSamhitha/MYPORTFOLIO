6d21fef3e3d84770e2bb651de0786cd07eb3acdf
HalaharviSamhitha <119795075+HalaharviSamhitha@users.noreply.github.com>
2024-03-28 03:10:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-ulet menuicon= document.querySelector('#menu-icon');
-let navbar= document.querySelector('.navbar');
-let sections= document.querySelectorAll('section');
-let navlinks= document.querySelectorAll('header nav a');
-document.getElementById('myresume').addEventListener('click', function(){
-  window.open('Resume Winner.pdf','_blank');
-})
+let menuicon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav a");
+document.getElementById("myresume").addEventListener("click", function () {
+  window.open("Resume Winner.pdf", "_blank");
+});
 
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offserHeight;
+    let id = sec.getAttribute("id");
 
-window.onscroll=()=>{
-    sections.forEach(sec => {
-      let top= window.scrollY;
-      let offset= sec.offsetTop-150;
-      let height= sec.offserHeight;
-      let id= sec.getAttribute('id');
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + " ]")
+          .classList.add("active");
+      });
+    }
+  });
+};
+menuicon.onclick = () => {
+  menuicon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 
-      if(top>=offset && top<offset+height){
-        let bar= document.getElementById("links");
-        let s= window.getComputedStyle(bar).visibility
-        if(s=='hidden'){
-         bar.classList.add("active"); 
-        }
-        else{
-          bar.classList.remove("active");
-}
-      }
 
