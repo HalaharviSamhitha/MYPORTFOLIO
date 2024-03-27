0cbb57c05db7d2ee8e3f426b3a62838c6d6235ff
HalaharviSamhitha <119795075+HalaharviSamhitha@users.noreply.github.com>
2024-03-28 00:20:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let navbar= document.querySelector('.navbar');
 let sections= document.querySelectorAll('section');
 let navlinks= document.querySelectorAll('header nav a');
 document.getElementById('myresume').addEventListener('click', function(){
-  window.open('HalaharviSamhitha_InternshalaResume.pdf','_blank');
+  window.open('Resume final 2.pdf','_blank');
 })
 
 
