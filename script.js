6c4298714a0c16793319dc355d9b350306a747d4
HalaharviSamhitha <119795075+HalaharviSamhitha@users.noreply.github.com>
2024-03-28 11:38:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ let menuicon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 let sections = document.querySelectorAll("section");
 let navlinks = document.querySelectorAll("header nav a");
+ document.getElementById('contactformsection').addEventListener('click', function(event) {
+  event.preventDefault(); // Prevents the default form submission
+
+  // Clear the message textarea
+  document.getElementById('message').value = '';
+
+  // Show alert message
+  alert('Message sent successfully!');
+});
+
 
 function openresume(){
   window.open('mydearresume.pdf','_blank');
